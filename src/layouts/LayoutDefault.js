2948494814c3b4a250e7c7019b328043e08e55fe
Calin Biberea <calinbiberea@gmail.com>
2022-06-22 21:46:16 +0300

    feat: include nicer modal style

--- src/layouts/LayoutDefault.js
+++ src/layouts/LayoutDefault.js
@@ -7,36 +7,38 @@ import Image from '../components/elements/Image';
 
 const LayoutDefault = ({ children }) => {
   const [menuBarOpen, setMenuBarOpen] = React.useState(false);
-  const values = [true, 'sm-down', 'md-down', 'lg-down', 'xl-down', 'xxl-down'];
 
   return (
     <>
       <Header navPosition="right" className="reveal-from-bottom" setMenuBarOpen={setMenuBarOpen} />
       <main className="site-content">
-        <Modal fullscreen={true} className="fullscreen-modal"
-          show={menuBarOpen}
-          onHide={() => setMenuBarOpen(false)}
-          animation={true}
-          dialogClassName="fullscreen-modal"
-        >
-          <div className="container modal-container">
-            <Modal.Header closeButton />
-
-            <Modal.Body style={{
-              maxHeight: '80vh',
-              overflowY: 'auto'
-            }}>
-              <Image
-                src={require('./../assets/images/menu.png')}
-                alt="Menu Bar"
-                width="100%"
-                height="90%" />
-            </Modal.Body>
-          </div>
-        </Modal>
         {children}
       </main>
       <Footer />
+      <Modal
+        show={menuBarOpen}
+        onHide={() => setMenuBarOpen(false)}
+        fullscreen={true}
+        animation={true}
+        scrollable={true}
+        backdrop={false}
+        centered={true}
+        className="fullscreen-modal"
+      >
+        <div className="container modal-container">
+          <Modal.Header closeButton closeVariant='white' />
+
+          <Modal.Body style={{
+            maxHeight: '75vh',
+            overflowY: 'auto'
+          }}>
+            <Image
+              src={require('./../assets/images/menu.png')}
+              alt="Menu Bar"
+            />
+          </Modal.Body>
+        </div>
+      </Modal>
     </>
   )
 };
